7777b0c51e9826bb40e61e8c5e0a33e1b43b3056
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2020-10-02 10:00:57 +0700

    add file exercise 21

---
+++ Exercise_21.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Exercise_21 {
+    public static void  main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int number1, number2;
+        System.out.println("Let's input value a from keyboard = ");
+        number1 = sc.nextInt();
+        System.out.println("Let's input value b from keyboard = ");
+        number2 = sc.nextInt();
+        System.out.println("a + b = " +(number1 + number2));
+        System.out.println("a - b = " +(number1 - number2));
+        System.out.println("a * b = " +(number1 * number2));
+        System.out.println("a / b = " +(number1 / number2));
+        System.out.println("a % b = " +(number1 % number2));
+    }
+}
