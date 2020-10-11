d3b84b574e7aafc747cbdfdc7f722a72568ce73f
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2020-10-11 13:35:05 +0700

    add Excercise_42

---
+++ Excercise_42.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Excercise_42 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input the height of the triangle from keyboard: ");
+        int number = sc.nextInt();
+         for(int i = 1; i <= number; i++){
+             for(int j = 1; j <= (2 * number -1); j++){
+                 if(Math.abs(number - j) <= (i - 1)){
+                     System.out.print(" * ");
+                 }else {
+                     System.out.print("   ");
+                 }             }
+             System.out.println("");
+         }
+
+    }
+}
