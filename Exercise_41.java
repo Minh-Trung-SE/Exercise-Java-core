9647b809737a89f2e33cdbb7539762d40b2c0953
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2020-10-14 01:32:25 +0700

    add file exercise 41 after fix

--- Exercise_41.java
+++ Exercise_41.java
@@ -5,13 +5,14 @@ public class Exercise_41 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please input number of fibonacci number you want number = ");
         int number = sc.nextInt();
-        int temp1 = 0, temp2 = 1, sum = 0;
+        int temp1 = 0, temp2 = 1, sum = 0, count = 0;
 
         do{
             System.out.print(temp1 + " ");
             sum = temp1 + temp2;
             temp1 = temp2;
             temp2 = sum;
-        }while (sum <= number);
+            count++;
+        }while (count < number);
     }
 }
