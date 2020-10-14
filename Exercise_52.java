fb231a194909832ba2604dded7e5262df58c9017
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2020-10-14 23:11:14 +0700

    add file 14/10/2020

--- Exercise_52.java
+++ Exercise_52.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Exercise_52 {
+import java.util.Scanner;
+
+public class Exercise_52 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number element of array: ");
+        int temp, element = sc.nextInt();
+        int[] array = new int[element];
+
+        //input element of array
+        for(int i = 0; i < element; i++){
+            System.out.print("Array[" + i + "] = ");
+            array[i] = sc.nextInt();
+        }
+        //check and removing element duplicate
+        for(int i = 0; i < (element - 1); i++){
+            if(array[i] == array[i+1]){
+                for(int j = i + 1; j < element; j++){
+                    array[j - 1] = array[j];
+                }
+                element--;
+            }
+        }
+        //output array after removing duplicate
+        System.out.println("Array after removing duplicate:");
+        for(int j = 0; j < element; j++){
+            System.out.print(array[j] + " ");
+        }
+    }
 }
