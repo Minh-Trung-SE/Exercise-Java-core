17af037236e4333f58b66a7b6f3ef4eb2a9c4002
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2020-10-14 23:22:56 +0700

    mod file exercise 51

--- Exercise_51.java
+++ Exercise_51.java
@@ -2,23 +2,9 @@ public class Exercise_51 {
     public static void main(String[] args) {
         int temp1 = 0, temp2 = 0, sum = 0, element = 0;
         System.out.println("List number Armstrong between 1 to 500:");
-        System.out.print(1 +" ");
-        for(int i = 153; i <= 500; i++){
-            temp2 = i;
-            sum = 0;
-            while (temp2 != 0){
-                element = temp2 % 10;
-                sum += Math.pow(element, 3);
-                temp2 /= 10;
-            }
-            if(i == sum){
-                System.out.print(i +" ");
-            }
-        }
-    }
-}
-           /* if(i <= 9){
-                if(i == i*i){
+        for(int i = 1; i <= 500; i++){
+            if(i <= 9){
+                if(i == i*1){
                     System.out.print(i +" ");
                 }
             }
@@ -27,4 +13,19 @@ public class Exercise_51 {
                 if(i == temp1){
                     System.out.print(i +" ");
                 }
-            } */
+            }
+            if (i >= 100) {
+                temp2 = i;
+                sum = 0;
+                while (temp2 != 0){
+                    element = temp2 % 10;
+                    sum += Math.pow(element, 3);
+                    temp2 /= 10;
+                }
+                if(i == sum){
+                    System.out.print(i +" ");
+                }
+            }
+        }
+    }
+}
