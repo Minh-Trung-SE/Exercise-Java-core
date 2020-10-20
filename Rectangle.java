ac0c4faf63f555dc933e0f6633efc4fa2ba43136
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2020-10-20 19:23:24 +0700

    add commet

--- Rectangle.java
+++ Rectangle.java
@@ -1,30 +1,30 @@
 class Rectangle {
     private float width; //attribute
     private float height; //attribute
-
+//method set and return width
     private float getWidth(){
         return this.width;
     }
-
+//method set and return height
     private float getHeight(){
         return this.height;
     }
-
+//method set and return perimeter
     private float getPerimeter(){
         return (width + height) * 2;
     }
-
+//method set and return area
     private float getArea() {
         return width * height;
     }
-
+//method check square
     private boolean checkSquare(){
         if(width == height){
             return true;
         }else
             return false;
     }
-
+//method display
     private void output(){
         System.out.println("Height: " + height);
         System.out.println("Width: " + width);
--- Students.java
+++ Students.java
@@ -1,12 +1,12 @@
 public class Students {
     private String name;
     private float gpa;
-
+//constructor
     public Students(String name, float gpa) {
         this.name = name;
         this.gpa = gpa;
     }
-
+//method check
     public boolean checkPass() {
         if(gpa>=1.5) {
             return  true;
@@ -14,30 +14,31 @@ public class Students {
             return false;
         }
     }
-
+//method print gpa
     public void displayGpa() {
 
         System.out.println("GPA:" + gpa);
     }
-
+//method print name
     public void displayName() {
         System.out.println("Name:" + name);
     }
-
-    public String getName() {
-        return name;
-    }
-
+//method set name
     public void setName(String name) {
         this.name = name;
     }
-
+//method return name
+    public String getName() {
+        return name;
+    }
+//method set gpa
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
+//method return gpa
     public float getGpa() {
         return gpa;
     }
 
-    public void setGpa(float gpa) {
 
-        this.gpa = gpa;
-    }
 }
